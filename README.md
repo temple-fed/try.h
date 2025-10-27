66ec5e0a787370118c1a27324fe83150bc5e412e
temple-fed <daniel.char@proton.me>
2025-10-27 21:35:13 +0200

    initial

---
+++ README.md
@@ -0,0 +1,7 @@
+# `try.h`
+
+This header was previously a github gist due to its small size. However the header has been made into a regular repository as a couple projects depend on the try.h utility and github gists are not that good.
+
+## What is `try.h` ?
+
+`try.h` is just a tiny C utility that provides try-catch *like* semantics by using the preprocessor. It does not use longjump or anything like that, just result checks.
---
+++ try.h
@@ -0,0 +1,39 @@
+#ifndef __TRY_H__
+#define __TRY_H__
+
+/*  try.h | temple-fed
+*   
+*   Emulates try catch behaviour using the preprocessor.
+*   Does not 'throw' using something like longjump, it simply just does result checking.
+*/
+
+#ifndef __TRY_SUCCESS__
+#   define __TRY_SUCCESS__   (0)
+#endif
+
+#define try                      do
+#define tryfailed(error)         (error != __TRY_SUCCESS__)
+#define trycall(error, result)   error = (result); if (tryfailed(error)) break
+#define catch(error)             while (0);        if (tryfailed(error))
+
+/*  Example usage:
+*   
+*   #include <try.h>
+*   
+*   int main(void){
+*      enum error_t error;
+*      
+*      try {
+*         // Try calling some functions.
+*         trycall(error, func1());
+*         trycall(error, func1());
+*      } catch (error){
+*         // Handle the error.
+*      }
+*      
+*      return 0;
+*   }
+*   
+*/
+
+#endif /*   __TRY_H__ EOF   */
